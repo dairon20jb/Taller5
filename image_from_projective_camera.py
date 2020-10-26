962913632760c4c9195ac45b67b67d6504562601
dairon20jb <dairon.barbosa@javeriana.edu.co>
2020-10-26 17:33:24 -0500

    Ac

--- image_from_projective_camera.py
+++ image_from_projective_camera.py
@@ -2,27 +2,31 @@ import os
 import cv2
 import json
 from camera_model1 import *
-def cargar_datos(ruta):
+def cargar_datos(ruta,para):
     with open(ruta) as contenido:
         cursos =json.load(contenido)
-        return (cursos.get('K'))
+        return (cursos.get(para))
 if __name__ == '__main__':
-    path = 'C:\PRUEBA\Calibrar'
-    path_file = os.path.join(path, 'calibration.json')
+    path = 'C:\PRUEBA\Taller5'
+    path_file = os.path.join(path, 'Punto2-2(Tablero1).json')
     # intrinsics parameters
     fx = 1000
     fy = 1000
-    width = 585
-    height = 1040
+    #1280*960
+    width = 1024
+    height = 720
     cx = width / 2
     cy = height / 2
     K = np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1.0]])
-    #K = cargar_datos(path_file)
+    #K = cargar_datos(path_file,'K')
     print(K)
     # extrinsics parameters
-    h =2
-    R = set_rotation(30, 0, 0)
-    t = np.array([0, -3, h])
+    h =cargar_datos(path_file,'h')
+    tilt=cargar_datos(path_file,'tilt')
+    pan=cargar_datos(path_file,'pan')
+    d=cargar_datos(path_file,'d')
+    R = set_rotation(tilt, pan, 0)
+    t = np.array([0, -d, h])
 
     # create camera
     camera = projective_camera(K, width, height, R, t)
@@ -57,5 +61,5 @@ if __name__ == '__main__':
     cv2.line(image_projective, (square_2D4[2][0], square_2D4[2][1]), (square_2D4[3][0], square_2D4[3][1]), (0, 0, 255), 3)
 
     cv2.imshow("Image", image_projective)
-    cv2.imwrite('Projective.png', image_projective)
+    cv2.imwrite('Punto2-2(Tablero1)R.png', image_projective)
     cv2.waitKey(0)
